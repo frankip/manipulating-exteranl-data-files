f066e81dacea25eb8a136cd482f4ab5a05ba4771
Francky <gwadafrank@gmail.com>
2022-03-15 02:21:03 +0300

    Update class methods and functions

--- files.py
+++ files.py
@@ -3,38 +3,27 @@ import pickle
 
 def load_data_from_csv_file():
     # import file
-    # with open('MOCK_DATA.json', 'r') as files:
+    with open('DATA.csv', 'r') as files:
+        data_content = files.readlines()[1:]
+        for i in data_content:
+            row = i.split(',')
+            new_contact = Employee(row[0], row[1], row[2],row[3], row[4], row[5], row[6])
+            new_contact.save()
 
-    # create a generator expressions
-    contents = (row for row in open('MOCK_DATA.csv', 'r'))
-    # print(contents)
+def write_to_file(payload):
+    with open('DATA.csv', 'a') as files:
+        print("____________", payload)
+        files.write(payload)
+        files.write('\n')
+        Employee.fetch_data()
 
-    # iterate through the generator to get each line
-    contents_list = (lines.rstrip().split(',') for lines in contents)
-    cols= next(contents_list)
-    # print("content list", list(contents_list)[0])
+# def delete_row_from_file(emp):
+#     with open('DATA.csv', 'ar') as files:
+#         print(emp.get_full_name())
+#         data_content = files.readlines()[1:]
+#         # for i in range(len(data_content)):
+#         print(data_content)
 
-    company_dicts = (dict(zip(cols, data)) for data in contents_list)
-   
-
-    # while True:
-
-        # yield company_dicts
-        # print("----------------------------------->")
-        # 
-        # read through the contents
-        # data_content = files.readlines()
-
-        # # iterate thtough the content
-        # for emp in data_content:
-        #     yield emp
-        # #     # print(f'{"emp"}----> {json.loads(emp)}')
-        # #     print('emp is ', json.dumps(emp))
-
-
-        # print("the data is ", next(company_dicts))
-    return company_dicts
-        
 
 class Employee():
     employees_list = list()
@@ -51,17 +40,106 @@ class Employee():
         def __str__(self):
             return self.first_name
 
+    def get_full_name(self):
+        return f"{self.first_name}  {self.last_name}"
+
+    def save(self):
+        """
+            the method saves the contact object into the contact list
+        """
+        # print("saving", len(self.contact_list))
+        self.employees_list.append(self)
+
+    def add_new(self,employee):
+        id = len(self.employees_list)+1
+        employee.id = str(id)
+        # pylod = "Ivonne,Bridell,ibridell9@comcast.net"
+        # first_name = , last_name, email = contact
+        print("dict",employee.__dict__.values())
+        pylod = ",".join(employee.__dict__.values())
+        # pylod = pickle.dumps(contact.__dict__.values())
+        print("load",pylod)
+        return write_to_file(pylod)
+    
+    @classmethod   
+    def update(cls, value):
+        """
+            the method saves the contact object into the contact list
+        """
+        # print("saving", len(self.contact_list))
+        payl= {
+
+        }
+        for contact in range(len(cls.employees_list)):
+            if cls.employees_list[contact].email == value:
+                print("s")
+                cls.employees_list[contact].email = "jleallu@state.tx"
+                return cls.employees_list[contact].get_full_name()
+
+
+    @staticmethod
+    def fetch_data():
+        '''
+        method that returns the contact list
+        '''
+        # print('len before', len(Employee.contact_list))
+        # load()
+
+        # print('len after', len(Contact.contact_list))
+        return [cont.get_full_name() for cont in Employee.employees_list]
+
     @classmethod
-    def fetch_data(self):
-        data = load_data_from_csv_file()
-        for row in data:
-            Employee.employees_list.append(row)
+    def check_existing(cls,email):
+        '''
+        Method that checks if a contact exists from the contact list.
+        Args:
+            number: Phone number to search if it exists
+        Returns :
+            Boolean: True or false depending if the contact exists
+        '''
+        for contact in cls.contact_list:
+            if contact.email == email:
+                print("s")
+                return contact.get_full_name()
 
-        print(self.employees_list[0])
-        return self.employees_list
+        return False
+
+    @classmethod
+    def find_by_email(cls,email):
+        '''
+        Method that takes in a number and returns a contact that matches that number.
+
+        Args:
+            number: Phone number to search for
+        Returns :
+            Contact of person that matches the number.
+        '''
+        emp = cls.check_existing(email)
+        if emp:
+            return emp
+        return False
 
 
+    @classmethod
+    def delete_employee(cls,email):
 
+        '''
+        delete_contact method deletes a saved employee from employee list
+        '''
+        for employee in cls.employees_list:
+            if employee.email == email:
+                print("--->", employee.get_full_name())
+                # delete_row_from_file(employee)
+                Employee.employees_list.remove(employee)
+                return cls.fetch_data()
+
+        return False
 
 if __name__=='__main__':
-    Employee.fetch_data()
+    load_data_from_csv_file()
+    new_emp = Employee('2','http://dummyimage.com/208x100.png/cc0000/ffffff','Rori','Kingerby','rkingerby1@feedburner.com','Training','13')
+    # new_emp.add_new(new_emp)
+    # print( Employee.update('rkingerby1@feedburner.com'))
+
+    print(Employee.delete_employee("rkingerby1@feedburner.com"))
+    print(Employee.fetch_data())
